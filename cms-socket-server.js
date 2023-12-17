28db40306ec6592b267b95a1f8e8aa59510b3d66
ForestChad <95200162+ForestChad@users.noreply.github.com>
2023-12-18 03:56:23 +0900

    fix broadcast issue

--- cms-socket-server.js
+++ cms-socket-server.js
@@ -1,51 +1,54 @@
 const net = require('net');
 
-const server = net.createServer();
-
-// Array to store the list of connected clients
+// Array to store clients
 const clients = [];
 
-// Function to broadcast a message to connected clients, excluding the sender
+// Create TCP server
+const server = net.createServer(socket => {
+  // When a new client is connected
+  console.log('Client connected');
+
+  // Store the client in the array
+  clients.push(socket);
+
+  // When a message is received from the client
+  socket.on('data', data => {
+    const message = data.toString().trim();
+    console.log(`Received message: ${message}`);
+
+    // Broadcast the message to all clients
+    broadcast(message, socket);
+  });
+
+  // When the client connection is terminated
+  socket.on('end', () => {
+    console.log('Client disconnected');
+
+    // Remove the client from the array
+    clients.splice(clients.indexOf(socket), 1);
+  });
+
+  // Handle errors
+  socket.on('error', err => {
+    console.error('Socket error:', err.message);
+    // Remove the client from the array on error
+    clients.splice(clients.indexOf(socket), 1);
+    socket.destroy();
+  });
+});
+
+// Function to broadcast a message to all clients
 function broadcast(message, sender) {
   clients.forEach(client => {
-    // Do not send the message to the client who sent it
+    // Send the message to all clients except the sender
     if (client !== sender) {
-      client.write(`${sender.remoteAddress}:${sender.remotePort} says: ${message}`);
+      client.write(`${message}\n`);
     }
   });
 }
 
-// Connection event handler for clients
-server.on('connection', socket => {
-  console.log(`Client connected: ${socket.remoteAddress}:${socket.remotePort}`);
-  
-  // Add the client to the array
-  clients.push(socket);
-
-  // Event handler when data is received from the client
-  socket.on('data', data => {
-    const message = data.toString().trim();
-    console.log(`Received from ${socket.remoteAddress}:${socket.remotePort}: ${message}`);
-    
-    // Broadcast the received message to all connected clients
-    broadcast(message, socket);
-  });
-
-  // Event handler for client disconnection
-  socket.on('end', () => {
-    console.log(`Client disconnected: ${socket.remoteAddress}:${socket.remotePort}`);
-    
-    // Remove the client from the array
-    clients.splice(clients.indexOf(socket), 1);
-  });
-});
-
-// Event handler when the server starts listening
-server.on('listening', () => {
-  const { address, port } = server.address();
-  console.log(`Server listening on ${address}:${port}`);
-});
-
-// Start the server on a specific port
+// Server listens on a specific port
 const PORT = 12562;
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
